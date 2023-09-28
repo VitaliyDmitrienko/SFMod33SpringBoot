2f71a840a47ecd1ee09576b9afcb6ae0880bd905
ДмитриенкоВВ <vddjack2008@yandex.ru>
2023-09-28 17:44:51 +0300

    1st.

--- src/main/java/org/example/SampleController.java
+++ src/main/java/org/example/SampleController.java
@@ -10,15 +10,33 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+    static String postString = null;
 
     // сообщаем сервису, что он должен работать по URL-адресу /hello
     // например, http://localhost:8080/hello
     // и говорим, что у него должно быть тело ответа (т.е. ответ не пустой)
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     @ResponseBody
     String helloCourse() {
         return "Hello SkillFactory!";
     }
+
+    @GetMapping("/oneanswer")
+    @ResponseBody
+    String something() {
+        return "One answer";
+    }
+    @PostMapping("/register")
+    void postSaver (@RequestParam String incomingString){
+        postString = incomingString;
+    }
+    @GetMapping("/responseanswer")
+    @ResponseBody
+    String responseSomething() {
+        if (postString == null || postString.isEmpty()) return "Not present";
+        else return("Present: " + postString);
+    }
+
 
     public static void main(String[] args) throws Exception {
         // метод, который запускает наше Spring-приложение,
