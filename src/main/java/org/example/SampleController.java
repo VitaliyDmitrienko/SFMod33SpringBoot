d963108a8b84f0538007af74a5b9951ce7ef807c
ДмитриенкоВВ <vddjack2008@yandex.ru>
2023-10-04 13:54:33 +0300

    3rd. Remove @RequestMapping("/users") to class header. Correct all method's headers @ to unification "curl naming manifest".

--- src/main/java/org/example/SampleController.java
+++ src/main/java/org/example/SampleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 // который сконфигурируется сам
 @Controller
 @RestController
+@RequestMapping("/users")
 @EnableAutoConfiguration
 public class SampleController {
     static String postString = null;
@@ -17,7 +18,7 @@ public class SampleController {
     // например, http://localhost:8080/hello
     // и говорим, что у него должно быть тело ответа (т.е. ответ не пустой)
 //    @RequestMapping("/")
-    @GetMapping("/")
+    @GetMapping("/hello")
 //    @ResponseBody
     String helloCourse() {
         return "Hello SkillFactory!";
@@ -28,16 +29,16 @@ public class SampleController {
     String something() {
         return "One answer";
     }
-    @PostMapping("/register")
+    @PostMapping
 //    @ResponseBody
     void postSaver (@RequestParam String incomingString){
         postString = incomingString;
     }
-    @GetMapping("/responseanswer")
+    @GetMapping
 //    @ResponseBody
     String responseSomething() {
-        if (postString == null || postString.isEmpty()) return "Not present";
-        else return("Present: " + postString);
+        if (postString == null || postString.isEmpty()) return "Record is not present/not yet init.";
+        else return("Record is present. User name is: " + postString);
     }
 
 
