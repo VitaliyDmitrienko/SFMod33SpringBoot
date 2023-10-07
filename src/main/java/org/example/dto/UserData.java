aa763f115aa298d2a8136273d128cc54d635597b
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-10-07 16:19:13 +0300

    6th. Add @Autowired + @Qualifier("userData") to dto/UserData.java

--- src/main/java/org/example/dto/UserData.java
+++ src/main/java/org/example/dto/UserData.java
@@ -2,10 +2,17 @@ package org.example.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import java.io.Serializable;
 
 @Data
 public class UserData implements Serializable {
+    @Autowired
+    @Qualifier("userData")
+    private UserData userData;
+
     @JsonProperty("id")
     private int userID;
 
